9202fe5799ddd96d9eb0712c19e907e1e3e51929
jax02 <39723691+jax02@users.noreply.github.com>
2021-08-09 22:52:12 +0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,61 @@
+const htmlEditor=CodeMirror(document.querySelector(".editor .code .html-code"),{
+    theme:"darcula",
+    lineNumbers:true,
+    mode:"xml"
+    
+    
+});
+htmlEditor.setSize("100%","100%");
+htmlEditor.setValue(
+    "<canvas" + " " +"\n" +"id=" + "\"" + "fractal" + "\"" +" " 
+    +"\n"+ "width=" + "\"" +"800" + "\"" +" " 
+    +"\n"+ "height=" + "\"" +"800" + "\""
+    +">" +"\n" +"</canvas>"
+    );
+
+// const cssEditor=CodeMirror(document.querySelector(".editor .code .css-code"),{
+//     lineNumbers:true,
+//     mode:"css"
+// });
+const jsEditor=CodeMirror(document.querySelector(".editor .code .js-code"),{
+    theme:"darcula",
+    lineNumbers:true,
+    mode:"javascript"
+});
+jsEditor.setSize("100%","100%");
+jsEditor.setValue("var canvas = " + "document.getElementById("+ "\"" + "fractal" + "\"" +");"
++"\n"+"var ctx = " + "canvas.getContext(" + "\'" + "2d" + "\'" +");"
++"\n"+"drawCircle("+"320,"+"160,"+"135);"
++"\n"+"ctx.strokeRect("+"0,"+"0,"+"640,"+"320);"
++"\n"+"\n"+"     function drawCircle("+"x,"+"y,"+"r){"
++"\n"+"         ctx.beginPath();"
++"\n"+"         ctx.arc("+"x,"+"y,"+"r,"+"0,"+"2*Math.PI);"
++"\n"+"         ctx.stroke();"
++"\n"+"        }"
+);
+
+
+
+
+
+//
+// document.querySelector("#run-btn").addEventListener('click',function(){
+//     let htmlCode= htmlEditor.getValue();
+//     console.log(htmlCode);
+//     alert("click");
+//     // let cssCode= "<style>" +cssEditor.getValue() + "</style>";
+//     // let jsCode= "<scri" + "pt>" +jsEditor.getValue() + "</scri" + "pt>";
+//     // let previewWindow = document.querySelector("preview-window").contentwindow.document;
+//     // previewWindow.open();
+//     // previewWindow.write(htmlCode + cssCode + jsCode);
+//     // previewWindow.close();
+// });
+document.querySelector("#btn-run").addEventListener("click",function(){
+        let htmlCode= htmlEditor.getValue();
+        // let cssCode= "<style>" +cssEditor.getValue() + "</style>";
+        let jsCode= "<scri" + "pt>" +jsEditor.getValue() + "</scri" + "pt>";
+        let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+        previewWindow.open();
+        previewWindow.write(htmlCode+jsCode);
+        previewWindow.close();
+});
