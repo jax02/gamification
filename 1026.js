7f084809aa0c78c5e048df5ca584b16529a6a498
jax02 <39723691+jax02@users.noreply.github.com>
2021-10-26 16:42:23 +0800

    1026updates

---
+++ 1026.js
@@ -0,0 +1,50 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCy4KAcDzQl25dpVXl6F64H5iv2fWsOJNI",
+    authDomain: "login-3d8d7.firebaseapp.com",
+    databaseURL: "https://login-3d8d7-default-rtdb.firebaseio.com",
+    projectId: "login-3d8d7",
+    storageBucket: "login-3d8d7.appspot.com",
+    messagingSenderId: "857894651276",
+    appId: "1:857894651276:web:3a48b9edcb6deae43ac2a9",
+    measurementId: "G-BRFBBS3VGR"
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+
+
+function save() {
+    // let a = login();
+    // console.log(a);
+    let htmlCode = htmlEditor.getValue();
+    let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
+    console.log(htmlCode);
+    console.log(jsCode);
+
+    database.ref('/').set({
+        a: 10262,
+        htmlCode: htmlCode,
+        jsCode: jsCode
+    });
+};
+
+function recover() {
+
+    let recoverHtmlCode = firebase.database().ref('htmlCode');
+    let recoverJsCode = firebase.database().ref('jsCode');
+    recoverHtmlCode.once('value', function (snapshot) {
+        let htmlData = snapshot.val();
+        console.log(htmlData);
+        recoverJsCode.once('value', function (snapshot) {
+            let jsData = snapshot.val();
+            console.log(jsData);
+
+            let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+            previewWindow.open();
+            previewWindow.write(htmlData + jsData);
+            previewWindow.close();
+        });
+    });
+
+
+};
--- code.js
+++ code.js
@@ -1,37 +1,37 @@
-const htmlEditor=CodeMirror(document.querySelector(".editor .code .html-code"),{
-    theme:"darcula",
-    lineNumbers:true,
-    mode:"xml"
-    
-    
+let htmlEditor = CodeMirror(document.querySelector(".editor .code .html-code"), {
+    theme: "darcula",
+    lineNumbers: true,
+    mode: "xml"
+
+
 });
-htmlEditor.setSize("100%","100%");
+htmlEditor.setSize("100%", "100%");
 htmlEditor.setValue(
-    "<canvas" + " " +"\n" +"id=" + "\"" + "fractal" + "\"" +" " 
-    +"\n"+ "width=" + "\"" +"400" + "\"" +" " 
-    +"\n"+ "height=" + "\"" +"320" + "\""
-    +">" +"\n" +"</canvas>"
-    );
+    "<canvas" + " " + "\n" + "id=" + "\"" + "fractal" + "\"" + " "
+    + "\n" + "width=" + "\"" + "400" + "\"" + " "
+    + "\n" + "height=" + "\"" + "320" + "\""
+    + ">" + "\n" + "</canvas>"
+);
 
 // const cssEditor=CodeMirror(document.querySelector(".editor .code .css-code"),{
 //     lineNumbers:true,
 //     mode:"css"
 // });
-const jsEditor=CodeMirror(document.querySelector(".editor .code .js-code"),{
-    theme:"darcula",
-    lineNumbers:true,
-    mode:"javascript"
+let jsEditor = CodeMirror(document.querySelector(".editor .code .js-code"), {
+    theme: "darcula",
+    lineNumbers: true,
+    mode: "javascript"
 });
-jsEditor.setSize("100%","100%");
-jsEditor.setValue("var canvas = " + "document.getElementById("+ "\"" + "fractal" + "\"" +");"
-+"\n"+"var ctx = " + "canvas.getContext(" + "\'" + "2d" + "\'" +");"
-+"\n"+"drawCircle("+"160,"+"150,"+"135);"
-+"\n"+"ctx.strokeRect("+"0,"+"0,"+"320,"+"300);"
-+"\n"+"\n"+"     function drawCircle("+"x,"+"y,"+"r){"
-+"\n"+"         ctx.beginPath();"
-+"\n"+"         ctx.arc("+"x,"+"y,"+"r,"+"0,"+"2*Math.PI);"
-+"\n"+"         ctx.stroke();"
-+"\n"+"        }"
+jsEditor.setSize("100%", "100%");
+jsEditor.setValue("var canvas = " + "document.getElementById(" + "\"" + "fractal" + "\"" + ");"
+    + "\n" + "var ctx = " + "canvas.getContext(" + "\'" + "2d" + "\'" + ");"
+    + "\n" + "drawCircle(" + "160," + "150," + "135);"
+    + "\n" + "ctx.strokeRect(" + "0," + "0," + "320," + "300);"
+    + "\n" + "\n" + "     function drawCircle(" + "x," + "y," + "r){"
+    + "\n" + "         ctx.beginPath();"
+    + "\n" + "         ctx.arc(" + "x," + "y," + "r," + "0," + "2*Math.PI);"
+    + "\n" + "         ctx.stroke();"
+    + "\n" + "        }"
 );
 
 
@@ -50,22 +50,42 @@ jsEditor.setValue("var canvas = " + "document.getElementById("+ "\"" + "fractal"
 //     // previewWindow.write(htmlCode + cssCode + jsCode);
 //     // previewWindow.close();
 // });
-document.querySelector("#btn-run").addEventListener("click",function(){
+document.querySelector("#btn-run").addEventListener("click", function () {
 
-        let htmlCode= htmlEditor.getValue();
-        // let cssCode= "<style>" +cssEditor.getValue() + "</style>";
-        let jsCode= "<scri" + "pt>" +jsEditor.getValue() + "</scri" + "pt>";
-        let previewWindow = document.querySelector("#preview-window").contentWindow.document;
-        previewWindow.open();
-        previewWindow.write(htmlCode+jsCode);
-        previewWindow.close();
-    
+    let htmlCode = htmlEditor.getValue();
+    // let cssCode= "<style>" +cssEditor.getValue() + "</style>";
+    let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
+    let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+    previewWindow.open();
+    previewWindow.write(htmlCode + jsCode);
+    previewWindow.close();
+
 });
 
-document.querySelector("#btn-reset").addEventListener("click",function(){
-    
-    let htmlCode= htmlEditor.getValue();
+document.querySelector("#btn-reset").addEventListener("click", function () {
+    htmlEditor.setValue(
+        "<canvas" + " " + "\n" + "id=" + "\"" + "fractal" + "\"" + " "
+        + "\n" + "width=" + "\"" + "400" + "\"" + " "
+        + "\n" + "height=" + "\"" + "320" + "\""
+        + ">" + "\n" + "</canvas>"
+    );
+    jsEditor.setValue("var canvas = " + "document.getElementById(" + "\"" + "fractal" + "\"" + ");"
+        + "\n" + "var ctx = " + "canvas.getContext(" + "\'" + "2d" + "\'" + ");"
+        + "\n" + "drawCircle(" + "160," + "150," + "135);"
+        + "\n" + "ctx.strokeRect(" + "0," + "0," + "320," + "300);"
+        + "\n" + "\n" + "     function drawCircle(" + "x," + "y," + "r){"
+        + "\n" + "         ctx.beginPath();"
+        + "\n" + "         ctx.arc(" + "x," + "y," + "r," + "0," + "2*Math.PI);"
+        + "\n" + "         ctx.stroke();"
+        + "\n" + "        }"
+    );
+
+
+    let htmlCode = htmlEditor.getValue();
     // let cssCode= "<style>" +cssEditor.getValue() + "</style>";
-    let jsCode= "<scri" + "pt>" +jsEditor.getValue() + "</scri" + "pt>";
-    let previewWindow2 = document.querySelector("#preview-window2").contentWindow.document;3
+    let jsCode = "<scri" + "pt>" + jsEditor.getValue() + "</scri" + "pt>";
+    let previewWindow = document.querySelector("#preview-window").contentWindow.document;
+    previewWindow.open();
+    previewWindow.write(htmlCode + jsCode);
+    previewWindow.close();
 });
